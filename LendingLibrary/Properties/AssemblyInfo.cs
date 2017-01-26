d34708acaea96e3facd9916ee99053a7b17e2f9a
mrdoowan <sduans@umich.edu>
2017-01-26 10:27:56 -0500

    editing Properties

--- LendingLibrary/Properties/AssemblyInfo.cs
+++ LendingLibrary/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LendingLibrary")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("UMich Lending Library")]
+[assembly: AssemblyDescription("Keeps track of inventory for residents")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Steven Duan")]
 [assembly: AssemblyProduct("LendingLibrary")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.1.0")]
+[assembly: AssemblyFileVersion("1.2.1.0")]
